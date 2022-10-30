b3e9d3aad688f3533afb2bd3133cca2fb39fd723
bongcoy <mpascaldewantara@gmail.com>
2022-10-30 18:24:23 +0700

    update coba

--- tp/server_1301204300.py
+++ tp/server_1301204300.py
@@ -2,7 +2,7 @@ from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 
 server = SimpleJSONRPCServer(('localhost', 5000))
 
-class Voting:
+class GameVoting:
 
     def __init__(self):
         self.vote_dict = {"FIFA 23": 0, "PES 2023": 0, "Valorant": 0, "Overwatch 2": 0, "Minecraft": 0}
@@ -17,13 +17,8 @@ class Voting:
     def show_candidates(self): 
         return self.vote_dict
         
-        
 
-# iseng coba inheritance
-class Voting_2(Voting): 
-    pass
-
-vote = Voting_2() # instansiasi objek
+vote = GameVoting() # instansiasi objek
 
 # register fungsi sama procedure
 server.register_function(vote.add_candidate)
